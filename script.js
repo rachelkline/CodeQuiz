adcef29754c66126e4bfc58b092a363b7496116c
Rachel Kline <rach3lkline@gmail.com>
2020-09-08 09:35:56 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,126 @@
+var timer = 60;
+var time = document.querySelector("#timer");
+var questionBox = document.querySelector(".card-title");
+var btnContainer = document.querySelector("#btnContainer");
+var index = 0;
+
+
+var questions = [
+    {
+        questionTitle: "What should appear at the very end of your JavaScript?",
+        a:["The <script>", "The END statement", "The </script>", "None of the above"],
+        correctAnswer: "The </script>"
+    },
+    {
+        questionTitle: "How does JavaScript store dates in a date object?",
+        a:["The number of hours since the internet first launced","The number of days since January 1st, 1900","The number of seconds since Netscape's public stock offering","The number of milliseconds since January 1st, 1970"],
+        correctAnswer: "The number of milliseconds since January 1st, 1970"
+    },
+    {
+        questionTitle: "What is the correct JavaScript syntax to write 'Hello World'?",
+        a:["System.out.printIn('Hello World')", "printIn('Hello World')","document.write('Hello World')","response.write('Hello World')"],
+        correctAnswer: "document.write('Hello World')"
+    },
+    {
+        questionTitle: "What does the <noscript> tag do?",
+        a:["Enclose text to be displayed by non-JavaScript browsers","Prevents scripts on the page from executing","Describes certain low-budget movies","None of the above"],
+        correctAnswer: "Enclose text to be displayed by non-JavaScript browsers"
+    },
+    {
+        questionTitle: "JavaScript entities start with _______ and end with _________.",
+        a:["Semicolon, colon","Semicolon, Ampersand","Ampersand, colon","Ampersand, semicolon"],
+        correctAnswer: "Ampersand, semicolon"
+    },
+    {
+        questionTitle: "Using _______ statement is how you test for a specific condition.",
+        a:["Select","If","Switch","For"],
+        correctAnswer: "If"
+    },
+    {
+        questionTitle: "Which tag(s) can handle mouse events in Netscape?",
+        a:["<IMG>","<A>","<BR>","<P>"],
+        correctAnswer: "<A>"
+    },
+]
+
+//questionBox.textContent 
+
+
+
+
+function startTimer() {
+    timer--;
+}
+
+function quesStart () {
+    var myTimer = setInterval(function(){
+    timer--;
+    time.textContent = timer + " left remaining"
+    if(timer === 0) {
+        clearInterval(myTimer);
+    }
+
+    },
+    1000);
+
+nextQues ();
+
+}
+function nextQues() {
+     questionBox.textContent = questions[index].questionTitle;
+        btnContainer.innerHTML = "";
+    questions[index].a.forEach(function(answer,i){
+        var aBtn = document.createElement("button");
+        aBtn.setAttribute("value", answer);
+        aBtn.textContent = answer;
+        aBtn.onclick = logicClick;
+        btnContainer.appendChild(aBtn);
+
+    })
+    
+}
+
+quesStart();
+
+function logicClick() {
+    if(this.value === questions[index].correctAnswer) {
+        index++;
+        nextQues()
+        
+    }
+
+
+}
+
+// // Pseudocode
+// if(startButtonCLicked) {
+//     displayQuestion();
+//     startTimer();
+// }
+
+// //Timer should be global
+// //Dave's idea
+// //Pseudocode
+
+// var currentQuestion = 0;
+
+// if(choice3Clicked) {
+//     if (questions[currentQuestion]
+//     [choice3Clicked.id] === questions
+//     [currentQuestion].answer) {
+//         //correct answer
+//     } else {
+//         //Wrong answer, you lose x amount of seconds
+//     }
+// }
+
+//     if(currentQuestion < 10) {
+//         //Go to the next question
+//         currentQuestion ++;
+//         displayQuestion()
+
+//     } else {
+//         //Game over
+//     }
+
+// question[0] ["choice3"]
